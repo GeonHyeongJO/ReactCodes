6d653195e187e576042300a29a04e9278da01741
GeonHyeongJo <paincry0421@gmail.com>
2022-02-11 15:34:34 +0900

    style 추가

--- TodoList/App.js
+++ TodoList/App.js
@@ -1,7 +1,29 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Platform, Button, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 import _ from 'underscore';
+import styled from 'styled-components/native';
+import Constants from 'expo-constants';
+
+const Row = styled.View`
+  flex-direction: row;
+`
+
+const Input = styled.TextInput`
+  width: 300;
+  border-color: #000;
+  border-bottom-width: 1;
+`
+
+const TodoItem = styled.View`
+  flex-direction: row;
+  width: 350;
+  justify-Content: space-between;
+`
+
+const CustomButton = styled.Button`
+
+`
 
 const str = 'string here';
 const replaceStr = str.replace( ' ', '%20' );
@@ -22,21 +44,28 @@ export default function App() {
   const remove = id => {
     setList( _.reject( list, item => item.id === id ) );
   }
+  let marginTop = 0;
+  if(Platform.OS === 'android'){
+    marginTop = Constants.statusBarHeight;
+  }
   return (
-    <View style={styles.container}>
-      <TextInput value={ content } 
-                 onChangeText={ text => setContent( text ) }
-                 style={{ width: 300 }}/>
-      <Button title="추가" onPress={ () => addItem() }/>
+    <SafeAreaView style={ [ styles.container, { marginTop } ]}>
+      <ScrollView>
+      <View style={ [ styles.row, { marginBottom: 12, width: 340, justifyContent:'space-between' } ] }>
+        <Input value={ content } 
+                  onChangeText={ text => setContent( text ) }/>
+        <Button title="추가" onPress={ () => addItem() }/>
+      </View>
       { list.map( item => (
-        <View key={ item.id } style={{flexDirection: 'row'}}>
+        <TodoItem key={ item.id }>
           <Text>{ item.content }</Text>
           <Button color={'#f00'} title="삭제" onPress={ () => { remove( item.id )} } />
-        </View>
+        </TodoItem>
       ))
       }
       <StatusBar style="auto" />
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -45,6 +74,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  row: {
+    flexDirection: 'row',
+  },
+  todoItem: {
+    width: 340,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+  },
+  inputStyle: {
+    width: 300, 
+    borderColor: "#000", 
+    borderBottomWidth: 1,
+  }
 });
